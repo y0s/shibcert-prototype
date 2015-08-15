c75764ce15afaf6a3dc4773c40eececd0e2914b2
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2015-08-16 05:55:11 +0900

    bundle exec rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shibcert::Application.config.secret_key_base = '9a1247d5af5fe367a6a6524388f5ae899925f1aed4ef0fc5e01a978c9b088f6f39f4e5768bbf6faf598d67212fca809a74826b52b818064308a7896eda392121'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shibcert::Application.config.session_store :cookie_store, key: '_shibcert_session'
