fc65048ee2124efb4260e3591274bcac60396c8e
Yoshitaka Morimura <morimura@debian-rails.kuins.net>
2015-10-08 18:27:22 +0900

    add user-based selection to index/show

--- app/controllers/certs_controller.rb
+++ app/controllers/certs_controller.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 class CertsController < ApplicationController
-  before_action :set_cert, only: [:show, :edit, :update, :destroy, :edit_name_remote]
-
+  before_action :set_cert, only: [:edit, :update, :destroy]
+  before_action :set_cert_of_user, only: [:show, :edit_name_remote]
   # GET /certs
   # GET /certs.json
   def index
-    @certs = Cert.all
+    @certs = Cert.where(user_id: current_user.id)
   end
 
   # GET /certs/1
@@ -21,13 +21,11 @@ class CertsController < ApplicationController
 
   # POST /certs/request_result
   def request_result
-    uid = SESSION_USER_DUMMY_ID
     # state: 0 => now requesting, 1 => issued, 2 => issued but expired,
     #        3 => invadidated, -1 => error
     # purpose_type: profile ID of
     #   https://certs.nii.ac.jp/archive/TSV_File_Format/client_tsv/ 
-
-    request_params = params.require(:cert).permit(:purpose_type).merge({user_id: uid, state: 0})
+    request_params = params.require(:cert).permit(:purpose_type).merge({user_id: current_user.id, state: 0})
     @cert = Cert.new(request_params)
     @cert.save
     @new_cert_id = @cert.id
@@ -46,8 +44,10 @@ class CertsController < ApplicationController
   def edit_name_remote
     #    @cert.update('name = ' + params[:name])
     #    @cert.update_attributes = {name: params[:name]}
-    @cert.attributes = params.require(:cert).permit(:name)
-    @cert.save
+    if @cert
+      @cert.attributes = params.require(:cert).permit(:name)
+      @cert.save
+    end
   end
 
   # POST /certs
@@ -89,14 +89,28 @@ class CertsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cert
-      @cert = Cert.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cert
+    @cert = Cert.find(params[:id])    
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+  def set_cert_of_user
+    # FIXME: fix for not found error
+    begin
+      mycert = Cert.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      mycert = nil
+    end
+    if mycert and mycert.user_id == current_user.id
+      @cert = mycert
+    else
+      @cert = nil
+    end
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
     def cert_params
       params.require(:cert).permit(:name, :get_at, :expire_at, :pin, :pin_get_at, :user_id, :cert_state_id, :cert_type_id, :purpose_type)
     end
