9f06704b25cec89e887f26b486329e833ef8c2aa
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2015-10-08 13:02:37 +0900

    modify: add provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818043001) do
+ActiveRecord::Schema.define(version: 20151007062745) do
 
   create_table "cert_states", force: true do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150818043001) do
     t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
   end
 
 end
